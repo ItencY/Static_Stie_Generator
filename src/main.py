6a0df8f86e6cf2ea57005998979fe740b4b611af
itency <itency@outlook.com>
2024-07-15 11:09:48 +0300

    Create and test class TextNode

--- src/main.py
+++ src/main.py
@@ -1,1 +1,8 @@
-print("hello world")
+from textnode import TextNode
+
+def main():
+    new_text_node = TextNode("This is text", "bold", "https://www.boot.dev")
+    print(new_text_node)
+
+
+main()
