649b97f582868846e63aa4cb11e46c9b0f2fa091
itency <itency@outlook.com>
2024-07-16 17:51:51 +0300

    create function split and links

--- src/inline_render.py
+++ src/inline_render.py
@@ -1,3 +1,4 @@
+import re
 from textnode import TextNode
 
 TEXT_TYPE_TEXT = "text"
@@ -25,4 +26,33 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             else:
                 split_nodes.append(TextNode(sections[i], text_type))
         new_nodes.extend(split_nodes)
+    return new_nodes
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[(.*?)\]\((.*?)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"\[(.*?)\]\((.*?)\)", text)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+    for old_node in old_nodes:
+        if old_node.text_type != TEXT_TYPE_TEXT:
+            new_nodes.append(old_node)
+            continue
+        original_text = old_node.text
+        images = extract_markdown_images(original_text)
+        if len(images) == 0:
+            new_nodes.append(old_node)
+            continue
+        for image in images:
+            sections = original_text.split(f"![{image[0]}]({image[1]})", 1)
+            if len(sections) != 2:
+                raise ValueError("Invalid markdown, image section not closed")
+            if sections[0] != "":
+                new_nodes.append(TextNode(sections[0], TEXT_TYPE_TEXT))
+            new_nodes.append(TextNode(image[0], TEXT_TYPE_IMAGE, image[1]))
+            original_text = sections[1]
+        if original_text != "":
+            new_nodes.append(TextNode(original_text, TEXT_TYPE_TEXT))
     return new_nodes
--- src/test_iline_render.py
+++ src/test_iline_render.py
@@ -1,6 +1,6 @@
 import unittest
 
-from inline_render import split_nodes_delimiter
+from inline_render import split_nodes_delimiter, extract_markdown_images, extract_markdown_links
 from textnode import TextNode, TEXT_TYPE_TEXT, TEXT_TYPE_BOLD, TEXT_TYPE_CODE, TEXT_TYPE_IMAGE, TEXT_TYPE_ITALIC, TEXT_TYPE_LINK
 
 
@@ -29,3 +29,48 @@ class TestInlineRender(unittest.TestCase):
         node = TextNode("This is text with a `code block` word", TEXT_TYPE_TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TEXT_TYPE_CODE)
         self.assertListEqual([TextNode("This is text with a ", TEXT_TYPE_TEXT), TextNode("code block", TEXT_TYPE_CODE), TextNode(" word", TEXT_TYPE_TEXT)], new_nodes)
+
+    def test_no_images(self):
+        text = "This is a text without any images"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [])
+    
+    def test_single_image(self):
+        text = "This is a text with a single image ![alt text](https://example.com/image.jpg)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [("alt text", "https://example.com/image.jpg")])
+
+    def test_multiple_images(self):
+        text = "This is a text with multiple images ![alt1](https://example.com/image1.jpg) and ![alt2](https://example.com/image2.jpg)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [("alt1", "https://example.com/image1.jpg"), ("alt2", "https://example.com/image2.jpg")])
+
+    def test_no_alt_text(self):
+        text = "This is a text with an image that has no alt text ![]https://example.com/image.gif)"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [])
+
+    def test_no_image_link(self):
+        text = "This is a text with an image that has no image link ![alt text]"
+        result = extract_markdown_images(text)
+        self.assertEqual(result, [])
+
+    def test_multiple_links(self):
+        text = "This is a [link](https://example.com) and [another link](https://another.com)"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [("link", "https://example.com"), ("another link", "https://another.com")])
+
+    def test_no_links(self):
+        text = "This is a text without any links"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [])
+
+    def test_single_link(self):
+        text = "This is a [single link](https://singlelink.com)"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [("single link", "https://singlelink.com")])
+
+    def test_links_with_different_text_and_urls(self):
+        text = "Here is a [link](https://example.com) and [another link](https://another.com)"
+        result = extract_markdown_links(text)
+        self.assertEqual(result, [("link", "https://example.com"), ("another link", "https://another.com")])
