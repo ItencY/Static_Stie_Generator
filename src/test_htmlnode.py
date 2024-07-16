131c10459fd31db074f843b01485cc238986782f
itency <itency@outlook.com>
2024-07-16 12:31:47 +0300

    add child class LeafNode and testing this class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_html_props(self):
@@ -13,3 +13,7 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("a", "Full Text", None, {"class": "color", "href": "https://yandex.ru"})
         self.assertEqual(node.props_to_html(), ' class="property" href="href="https://boot.dev"')
     """
+
+    def test_html_no_children(self):
+        node = LeafNode("div", "Full text")
+        self.assertEqual(node.to_html(), "<div>Full text</div>")
