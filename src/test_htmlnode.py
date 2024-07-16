1aeb152ebef5a7aa140c5b67dafc07468ee78703
itency <itency@outlook.com>
2024-07-16 12:48:57 +0300

    add child class ParentNode and testing this class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_html_props(self):
@@ -17,3 +17,8 @@ class TestHTMLNode(unittest.TestCase):
     def test_html_no_children(self):
         node = LeafNode("div", "Full text")
         self.assertEqual(node.to_html(), "<div>Full text</div>")
+
+    def test_to_html_with_children(self):
+        child_node = LeafNode("span", "child")
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(parent_node.to_html(),"<div><span>child</span></div>")
