d2c495702f60b2bbce68b648b5bcd376f7d7d780
itency <itency@outlook.com>
2024-07-17 10:58:39 +0300

    create dir static and add index.css and rivendell.png

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from textnode import TextNode
 from htmlnode import HTMLNode
 
+DIR_PATH_STATIC = "./static"
+DIR_PATH_PUBLIC = "./public"
+
 def main():
     new_text_node = TextNode("This is text", "bold", "https://www.boot.dev")
     print(new_text_node)
