c642b189b1eb794c7db71da3bbd5d73b0fe5548b
itency <itency@outlook.com>
2024-07-17 11:22:33 +0300

    create page_generator

---
+++ src/page_generation.py
@@ -0,0 +1,9 @@
+import os
+
+
+def extract_title(markdown):
+    lines = markdown.split("\n")
+    for line in lines:
+        if line.startswith("# "):
+            return line[2:]
+    raise ValueError("No title")
